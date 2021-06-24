5c360533593a5c258a20753e6cf7a9984fb24859
邱致豪 <s1071626@gm.pu.edu.tw>
2021-06-24 19:23:02 +0800

    第三題 Exam/About.cshtml 完成

---
+++ FinalExam410716267/Controllers/ExamController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FinalExam410716267.Controllers
+{
+    public class ExamController : Controller
+    {
+        // GET: Exam
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            @ViewBag.name = "邱致豪";
+            return View();
+        }
+    }
+}
